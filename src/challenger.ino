321fd0f31e8ba3987b8ed312122d94dfe790930b
Jindrich Vavruska <jindrich@vavruska.cz>
2023-06-17 10:29:03 +0200

    Sample test, sending AR six times with extreme weighting and dah/dit

--- src/challenger.ino
+++ src/challenger.ino
@@ -3,7 +3,7 @@
 int phase = 0 ;
 void setup() {
   keyingInterface.init();
-  keyingInterface.setTiming(25, 400, 45);
+  keyingInterface.setTiming(18, 500, 20);
   keyingInterface.sendElement( DIT );
 }
 
@@ -11,21 +11,25 @@ void loop() {
   KeyingStatus s = keyingInterface.service() ;
   ElementType e ;
 
-  if( phase > 48 ) return ;
+  if( phase >= 48 ) return ;
   if( s.busy == IDLE || s.nextElement == NO_ELEMENT ) {
-    switch (phase++ % 4)
+    switch (phase++ % 8)
     {
     case 0:
+    case 3:
+    case 5:
       e = DIT;
       break;
     case 1:
+    case 4:
       e = DAH;
       break;
     case 2:
+    case 6:
       e = CHARSPACE;
       break;
-    case 3:
-      e = NO_ELEMENT;
+    case 7:
+      e = WORDSPACE;
     }
     keyingInterface.sendElement(e);
   }
