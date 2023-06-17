04f6dc47e8b43c199ac92965af1a326eed6ada20
Jindrich Vavruska <jindrich@vavruska.cz>
2023-06-17 10:14:12 +0200

    testing loop: dit-dah-space repeated

--- src/challenger.ino
+++ src/challenger.ino
@@ -3,11 +3,30 @@
 int phase = 0 ;
 void setup() {
   keyingInterface.init();
+  keyingInterface.setTiming(25, 400, 45);
+  keyingInterface.sendElement( DIT );
 }
 
 void loop() {
-  if( phase > 100 ) return; 
-  int bip = 1 & phase++ ;
-  keyingInterface.setTone( bip * 1000 );
-  delay(50);
+  KeyingStatus s = keyingInterface.service() ;
+  ElementType e ;
+
+  if( phase > 48 ) return ;
+  if( s.busy == IDLE || s.nextElement == NO_ELEMENT ) {
+    switch (phase++ % 4)
+    {
+    case 0:
+      e = DIT;
+      break;
+    case 1:
+      e = DAH;
+      break;
+    case 2:
+      e = CHARSPACE;
+      break;
+    case 3:
+      e = NO_ELEMENT;
+    }
+    keyingInterface.sendElement(e);
+  }
 }
